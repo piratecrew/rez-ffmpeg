d64784ca5d2ca956ac1b4038ec524fc522a5428e
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2023-03-06 15:16:27 +0100

    Refactor for ffmpeg-5 and more structured cmake

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "ffmpeg"
 
-version = "2.8.4"
+version = "5.1"
 
 description = \
     """
@@ -12,6 +12,17 @@ variants = [
 ]
 
 uuid = "repository.ffmpeg"
+
+build_command = """
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$REZ_BUILD_INSTALL_PATH $REZ_BUILD_SOURCE_PATH;
+if [[ $REZ_BUILD_INSTALL -eq 1 ]];
+then
+    cmake --build $REZ_BUILD_PATH -j11
+    cmake --install $REZ_BUILD_PATH -j11
+else
+    echo we wont even build if you are not running install as we have several external projects that would install during buildstep;
+fi
+"""
 
 def commands():
     env.CMAKE_MODULE_PATH.append("{root}/cmake")
