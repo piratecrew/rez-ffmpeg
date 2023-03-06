60fb8bfaec13b7d5ac0bf41c8f0efc4d66bdab46
Tommy Hinks <tommy.hinks@ilpvfx.com>
2023-03-06 20:54:49 +0100

    Fix python warnings, add note on how to install freetype.

--- dependencies/post_install.py
+++ dependencies/post_install.py
@@ -5,28 +5,28 @@ import shutil
 
 def main(args):
     if not len(args)==3:
-        print "Not enough arguments."
+        print("Not enough arguments.")
         return
     source_path = args[1]
     install_path = args[2]
-    print "Source:", source_path
-    print "Install:", install_path
+    print("Source:", source_path)
+    print("Install:", install_path)
     lib_source_path = os.path.join(source_path, "DEPENDENCIES_PREFIX", "lib")
     lib_path = os.path.join(install_path, "lib")
-    print "Copy all dependency libs."
+    print("Copy all dependency libs.")
     for a in os.listdir(lib_source_path):
         if not a.endswith(".a"):
             continue
         lib_file = os.path.join(lib_source_path, a)
         shutil.copy(lib_file, os.path.join(lib_path, a))
-        print "Copying:", a
+        print("Copying:", a)
     pkgconfig_root = os.path.join(install_path, "lib", "pkgconfig")
     for pc in os.listdir(pkgconfig_root):
         pkgconfig_file = os.path.join(pkgconfig_root, pc)
         if not pkgconfig_file.endswith('.pc'):
             continue
         pkgconfig_tmp_file = os.path.join(pkgconfig_root, '%s.tmp' % (pc))
-        print "Processing:", pkgconfig_file
+        print("Processing:", pkgconfig_file)
         with open(pkgconfig_file, 'r') as pkg_file:
             with open(pkgconfig_tmp_file, 'w') as tmp_file:
                 for line in pkg_file.readlines():
